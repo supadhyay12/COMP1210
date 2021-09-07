98059c0b5fc51016f78c558f6da82beb9b1571a7
supadhyay12 <82346127+supadhyay12@users.noreply.github.com>
2021-09-07 16:49:57 -0500

    Add files via upload

---
+++ Project1/MyLifeGoals.java
@@ -0,0 +1,31 @@
+/** 
+ * Course: COMP1210 - Section 011 
+ *
+ * @author Shanti Upadhyay
+ * @version Date Due: Friday, August 31
+ */
+public class MyLifeGoals
+{
+  /**
+    * Prints course information to std output.
+    *
+    * @param args Command line arguments (not used).
+    */
+    
+   public static void main(String []args)
+   {
+      System.out.println("Shanti Upadhyay");
+      System.out.println();
+      System.out.println("My short-term goal is being able to finish my first "
+         + "Auburn with a 3.5 GPA. My overall GPA in high school was lower "
+         + "than my actual potential, so college is supposed to be a do-over.");
+      System.out.println("My medium-life goals include me graduating from "
+         + "Auburn University and having a job in the computer science "
+         + "field right after undergraduate school. If possible, even during.");
+      System.out.println("My long-term life goals include me getting married, "
+         + "having children, and living in LA with a successful job "
+         + "that can support our lifestyle.");
+      
+   }
+      
+}
